5d1e29e813e4411eb13d36b8a601ab2a0dd1aa07
KyeoungLim <kklim99@hufc.ac.kr>
2024-05-30 13:28:07 +0900

    i revise shipping_tags.py in research, furniture mean delivery date is 5, so i changed function number.

--- shipment/templatetags/shipping_tags.py
+++ shipment/templatetags/shipping_tags.py
@@ -6,9 +6,9 @@ register = template.Library()
 @register.filter
 def shipment_status(delivery):
     expected_days = (delivery.delivery_date - delivery.order.shipment_date).days
-    if expected_days < 0:
+    if expected_days < 5:
         return "early"
-    elif expected_days == 0:
+    elif expected_days == 5:
         return "fine"
     else:
         return "delay"

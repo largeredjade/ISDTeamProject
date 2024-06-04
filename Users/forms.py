30b73ecd9fea9b9ec26fb8c2c2608bbe5050b21c
largerejade <lsj31200@naver.com>
2024-06-04 13:24:42 +0900

    login 진짜 기능 완성

--- Users/forms.py
+++ Users/forms.py
@@ -1,9 +1,13 @@
 from django import forms
 from .models import Users
+from django.contrib.auth.forms import AuthenticationForm
 
 class UserRegistrationForm(forms.ModelForm):
-    user_password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput)
 
     class Meta:
         model = Users
-        fields = '__all__'
+        fields = ['user_id', 'user_address', 'user_address', 'user_phoneNumber', 'user_email','password', 'user_name', 'user_role', 'user_enterYear']
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.EmailField(label='Email', widget=forms.EmailInput(attrs={'autofocus': True}))
--- Users/views.py
+++ Users/views.py
@@ -1,10 +1,16 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from .forms import UserRegistrationForm
+from django.contrib.auth.views import LoginView
+from .forms import UserRegistrationForm, CustomAuthenticationForm
 from .models import Users
 
 class SignUpView(CreateView):
     model = Users
     form_class = UserRegistrationForm
     success_url = reverse_lazy("login")
-    template_name = "html/signup.html"
+    template_name = "registration/signup.html"
+
+class CustomLoginView(LoginView):
+    template_name = 'registration/login.html'
+    authentication_form = CustomAuthenticationForm
+    redirect_authenticated_user = True

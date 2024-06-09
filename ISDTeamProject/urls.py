8774a048ffd0b5a52ef97f1c673beb2c20db0a69
Flakiess <shermancollins03@gmail.com>
2024-06-09 16:34:57 +0900

    "Updated products/urls.py and products/views.py files, added new view and URL pattern for product update, and modified existing views and URL patterns."

--- ISDTeamProject/urls.py
+++ ISDTeamProject/urls.py
@@ -8,10 +8,11 @@ from ISDTeamProject import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('Users.urls')),
+    path('products/', include('products.urls')),
     path('delivery/', include('deliveries.urls')),
     path('shipment/', include('shipment.urls')),
     path('supplier/', include('suppliers.urls')),
-    path('hompage/',views.homepage, name='homepage'),
+    path('homepage/',views.homepage, name='homepage'),
     #path('accounts/', include('django.contrib.auth.urls')),
 
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from.models import Product
+from .forms import ProductForm
 
-# Create your views here.
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'html/product_list.html', {'products': products})
+
+def product_create(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')  # redirect to the product list page
+    else:
+        form = ProductForm()
+    return render(request, 'html/product_create.html', {'form': form})
+
+def product_update(request, product_id):
+    #... view logic here...
+    return render(request, 'html/product_update.html')
+
+def product_delete(request, pk):
+    product = Product.objects.get(pk=pk)
+    product.delete()
+    return redirect('product_list')  # redirect to the product list page

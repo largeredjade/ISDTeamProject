5d68dcc1bd41b46a07f553aeefb8bf9abcb35221
largerejade <lsj31200@naver.com>
2024-06-10 17:03:00 +0900

    make urls

--- orders/forms.py
+++ orders/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Order
+from .models import Order, OrderDetail
 
 class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('order_id', 'order_date', 'order_status','user','order_totalcost','expected_arrivaldate')  # specify the fields to include in the form
+
+class OrderDetailForm(forms.ModelForm):
+    class Meta:
+        model = OrderDetail
+        fields = '__all__'

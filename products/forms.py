26ee692f2b440f249ffb08850644767af7dfb689
KyeoungLim <kklim99@hufc.ac.kr>
2024-06-13 13:29:11 +0900

    24.06.13 project presentation

--- products/forms.py
+++ products/forms.py
@@ -4,4 +4,4 @@ from .models import Product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ('product_name', 'product_sellingprice', 'product_volume', 'product_weight', 'product_location', 'maintenance_cost', 'product_qty', 'product_minQty')
+        fields = '__all__'
